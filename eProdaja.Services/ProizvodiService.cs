edd354dde384e3073315d20b7ee9c1940fb6d6f8
ilhanneziric <ilhan.neziric@edu.fit.ba>
2022-07-05 00:07:17 +0200

    V1

---
+++ eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        public List<Proizvodi> ProizvodiList = new List<Proizvodi>() { new Proizvodi() { Id = 1, Naziv = "Laptop" }, new Proizvodi() { Id = 2, Naziv = "Miš" } };
+        public IEnumerable<Proizvodi> Get()
+        {
+            ProizvodiList.Add(new Proizvodi() { Id = 3, Naziv = "Test DI" });
+            return ProizvodiList;
+        }
+
+        public Proizvodi GetById(int id)
+        {
+            return ProizvodiList.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
